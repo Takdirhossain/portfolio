0d6445bf42a92c57699f1d4c9c074882926b228c
Takdir Hossain <takdirhossain35@gmail.com>
2025-04-22 23:42:15 +0600

    Updates

---
+++ _next/static/chunks/app/testimonials/page-64c480d227fafd43.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[158],{4906:function(e,r,t){Promise.resolve().then(t.bind(t,1493))},1493:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return c}});var n=t(9268),s=t(6006),o=t(6008);function c(){let e=(0,o.useRouter)();return(0,s.useEffect)(()=>{e.replace("/showcase")},[e]),(0,n.jsx)("div",{className:"min-h-screen flex items-center justify-center",children:(0,n.jsx)("p",{className:"text-xl",children:"Redirecting to showcase..."})})}},3177:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(6006),s=Symbol.for("react.element"),o=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,t){var n,f={},i=null,l=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(l=r.ref),r)o.call(r,n)&&!u.hasOwnProperty(n)&&(f[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===f[n]&&(f[n]=r[n]);return{$$typeof:s,type:e,key:i,ref:l,props:f,_owner:c.current}}r.jsx=f,r.jsxs=f},9268:function(e,r,t){"use strict";e.exports=t(3177)},6008:function(e,r,t){e.exports=t(794)}},function(e){e.O(0,[253,769,744],function(){return e(e.s=4906)}),_N_E=e.O()}]);
